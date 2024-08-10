0231dd37081df56deea8811c439d65f074dd2085
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-10 23:32:28 +0700

    created angular project

---
+++ shopapp-angular/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent  } from './home/home.component';
+// import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+   HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class AppModule { }
