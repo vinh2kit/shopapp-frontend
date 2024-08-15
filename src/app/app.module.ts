5335d55c12e9b247b541da1d1d40ab36c05247d4
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-16 00:18:28 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent  } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { RegisterComponent } from './register/register.component';
 // import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-   HomeComponent
+   HomeComponent,
+   HeaderComponent,
+   FooterComponent,
+   DetailProductComponent,
+   LoginComponent,
+   OrderComponent,
+   OrderConfirmComponent,
+   RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    HomeComponent
+    // HeaderComponent
+    // FooterComponent
+    // DetailProductComponent
+    // LoginComponent
+    // OrderComponent
+    // OrderConfirmComponent
+    // RegisterComponent
+  ]
 })
 export class AppModule { }
