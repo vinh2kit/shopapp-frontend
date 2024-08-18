2ed9dbdcafb7086bea992820469979aaf0a0404b
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-19 06:07:49 +0700

    upload first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent  } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { DetailProductComponent } from './detail-product/detail-product.component';
-import { LoginComponent } from './login/login.component';
-import { OrderComponent } from './order/order.component';
-import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
-import { RegisterComponent } from './register/register.component';
-// import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrderConfirmComponent } from './components/order-confirm/order-confirm.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { 
+  HttpClientModule, 
+  HTTP_INTERCEPTORS 
+} from '@angular/common/http';
+import {TokenInterceptor} from './interceptors/token.interceptor'
+
 
 @NgModule({
-  declarations: [
-   HomeComponent,
-   HeaderComponent,
-   FooterComponent,
-   DetailProductComponent,
-   LoginComponent,
-   OrderComponent,
-   OrderConfirmComponent,
-   RegisterComponent
+  declarations: [    
+    HomeComponent, 
+    HeaderComponent,
+    FooterComponent, 
+    DetailProductComponent, 
+    OrderComponent, 
+    OrderConfirmComponent, 
+    LoginComponent, 
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [
-    HomeComponent
-    // HeaderComponent
-    // FooterComponent
-    // DetailProductComponent
-    // LoginComponent
-    // OrderComponent
-    // OrderConfirmComponent
+    HomeComponent,
+    //DetailProductComponent,
+    // OrderComponent,
+    //OrderConfirmComponent,
+    // LoginComponent,
     // RegisterComponent
   ]
 })
